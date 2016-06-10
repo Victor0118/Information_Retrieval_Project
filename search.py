2749c2d29f725f1005855cf14de42f9ad2bec70e
qiuyingyue <qwinny@163.com>
2016-06-10 13:17:13 +0800

    Add files via upload
    update Class SearchEngine adding  functions called search_bool()
    Add class exprNode and parser which is used by function search_bool()

--- search.py
+++ search.py
@@ -4,6 +4,8 @@ import scipy.sparse as sp
 import scipy.sparse.sparsetools as sptools
 import logging
 from collections import defaultdict
+from token import RIGHTSHIFTEQUAL
+from Tkconstants import LEFT
 
 
 logger = logging.getLogger(__name__)
@@ -315,8 +317,10 @@ class Index(object):
                 docs_indices = docs_with_term
             else:
                 docs_indices = set(docs_indices) & set(docs_with_term)
+        #print "debuging:docs_indices",list(docs_indices)
         return list(docs_indices)
 
+        
 class SearchEngine(object):
     """Search engine for objects that can be indexed.
 
@@ -400,6 +404,57 @@ class SearchEngine(object):
         search_results.sort(key=lambda x: x.score, reverse=True)
         return search_results[:n_results]
 
+    def search_bool(self,query,n_results=10):
+        """return all documents that satisfy the demand given a bool expression
+        Assumptions:
+            1)we take 'and' 'or' 'not' as conjunction word
+        Args:
+          query (str): String of bool expression.
+          n_results (int): Desired number of results.
+
+        Returns:
+          list of  docs indexes
+        """
+        terms=query.lower().split()
+        #dealing with branket 
+        for term_index,term in enumerate(terms):
+            branket=''
+            if (term!='(' and term!=')'):
+                a=term.find('(')
+                b=term.find(')')
+                if(a!=-1 and b==-1):
+                    branket='('
+                elif(a==-1 and b!=-1):
+                    branket=')'
+                elif (a!=-1 and b!=-1 and a<b):
+                    branket='('
+                elif (a!=-1 and b!=-1 and a>b):
+                    branket=')'        
+            if branket!='':
+                c_index=term.find(branket)
+                #print c_index
+                terms.remove(term)
+                if c_index==0:
+                    terms.insert(term_index,branket)
+                    terms.insert(term_index+1,term.strip().lstrip(branket).rstrip(branket))
+                elif c_index==len(term)-1:
+                    terms.insert(term_index,term.strip().lstrip(branket).rstrip(branket))
+                    terms.insert(term_index+1,branket)
+                elif c_index!=-1:
+                    tt=term.split(branket)
+                    terms.insert(term_index,tt[0])
+                    terms.insert(term_index+1,branket)
+                    terms.insert(term_index+2,tt[1])
+        print "debug info:",terms
+        
+        expRoot=parser(terms).parse()       
+        #docs_indiex = self.index.search_terms(term)
+        #expRoot.printout()       
+        search_results = expRoot.calc(self.index,self.count())
+        return search_results[:n_results]
+   
+        
+    
     def count(self):
         """Return number of objects a
         eady in the index.
@@ -407,3 +462,111 @@ class SearchEngine(object):
           int: Number of documents indexed.
         """
         return len(self.objects)
+depth=0         
+class exprNode(object):
+    '''Class of bool expression tree'''
+    def __init__(self,value):
+        self.op_value=value
+        self.leftchild=None
+        self.rightchild=None             
+    def linkNode(self,left,right):
+        self.leftchild=left 
+        self.rightchild=right 
+        return self  
+    def printout(self):
+        global depth
+        leftnode=self.leftchild
+        rightnode=self.rightchild
+        if leftnode!=None:            
+            depth=depth-1
+            leftnode.printout()  
+            depth=depth+1
+        print self.op_value,depth
+        if rightnode!=None:            
+            depth=depth-1
+            rightnode.printout()
+            depth=depth+1
+             
+        #if leftres==None and rightres==None:
+        #    print self.op_value,depth      
+    def calc(self,index,docs_num):
+    '''calculate the docs indexes according to bool expression tree'''
+        leftnode=self.leftchild
+        rightnode=self.rightchild
+        if self.op_value=='and':
+            
+            leftdocs_indices=self.leftchild.calc(index,docs_num)
+            rightdocs_indices=self.rightchild.calc(index,docs_num)
+            res=set(leftdocs_indices) & set(rightdocs_indices)
+                    
+            return list(res)
+        elif self.op_value=='or': 
+            leftdocs_indices=self.leftchild.calc(index,docs_num)
+            rightdocs_indices=self.rightchild.calc(index,docs_num)
+            res=set(leftdocs_indices)| set(rightdocs_indices)
+            return list(res)
+        elif self.op_value=='not':          
+            leftdocs_indices=range(docs_num)
+            rightdocs_indices=self.rightchild.calc(index,docs_num)
+            res=set(leftdocs_indices)- set(rightdocs_indices)
+                
+            return list(res)
+        else:
+            res=index.search_terms(self.op_value.split())
+            print "debug info:",self.op_value.split(),res
+            return res
+class parser(object):
+    '''parse bool expression'''
+    def __init__(self,terms):
+        self.terms=terms
+        self.i=0
+        self.token=self.terms[self.i]
+    def parse(self):
+        return self.exp()
+    def match(self,expectedToken):
+        token=self.terms[self.i]
+        if token==expectedToken:
+            self.i=self.i+1
+            if(self.i!=len(self.terms)):
+                self.token=self.terms[self.i]
+            return exprNode(token)
+        else:
+            print 'systax error error',token
+    def factor(self):
+        if(self.token=='not'):
+            op=self.match('not')
+            right=self.negation()
+            return op.linkNode(None, right)
+        else:
+            return self.negation()
+    def negation(self):
+        if(self.token=='('):
+            self.match('(')
+            exp=self.exp()
+            self.match(')')
+            return exp
+        else:
+            return self.words()
+    def words(self):
+        tokenlist=[]
+        while(self.token!='(' and self.token !=')' and self.token !='and' and self.token != 'or' and self.token!='not'):
+            '''for single word'''
+            return self.match(self.token)#one words 
+            '''for multiple words query
+            tokenlist.append(self.token) 
+            self.match(self.token)
+        return exprNode(tokenlist)'''           
+    def term(self):
+        left=self.factor()
+        while self.token=='and':
+            op=self.match(self.token )
+            right=self.factor()
+            left=op.linkNode(left, right)  
+        return left          
+    def exp(self):
+        left=self.term()
+        while(self.token=='or'):
+            op=self.match(self.token)
+            right=self.term()
+            left=op.linkNode(left, right)
+        return left                
